5631a4c20ac54cab1a5d7598639529c434872384
frankwu83 <frankandsnow@hotmail.com>
2013-08-11 16:10:08 +0800

    New Update
    New Update

---
+++ Senparc.Weixin.MP/CommonAPIs/CommonApi.Menu.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web.Script.Serialization;
+using Senparc.Weixin.MP.Entities;
+using Senparc.Weixin.MP.Entities.Menu;
+using Senparc.Weixin.MP.HttpUtility;
+
+namespace Senparc.Weixin.MP.CommonAPIs
+{
+    public partial class CommonApi
+    {
+        /// <summary>
+        /// 创建菜单
+        /// </summary>
+        /// <param name="accessToken"></param>
+        /// <param name="buttonData">菜单内容</param>
+        /// <returns></returns>
+        public static WxJsonResult CreateMenu(string accessToken, ButtonGroup buttonData)
+        {
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            var jsonString = js.Serialize(buttonData);
+            CookieContainer cookieContainer = null;// new CookieContainer();
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                var bytes = Encoding.UTF8.GetBytes(jsonString);
+                ms.Write(bytes, 0, bytes.Length);
+                ms.Seek(0, SeekOrigin.Begin);
+
+                var url = string.Format("https://api.weixin.qq.com/cgi-bin/menu/create?access_token={0}", accessToken);
+                var result = Post.PostGetJson<WxJsonResult>(url, cookieContainer, ms);
+                return result;
+            }
+        }
+
+        #region GetMenu
+        /// <summary>
+        /// 获取单击按钮
+        /// </summary>
+        /// <param name="objs"></param>
+        /// <returns></returns>
+        private static SingleButton GetSingleButtonFromJsonObject(Dictionary<string, object> objs)
+        {
+            var sb = new SingleButton()
+            {
+                key = objs["key"] as string,
+                name = objs["name"] as string,
+                type = objs["type"] as string
+            };
+            return sb;
+        }
+
+        /// <summary>
+        /// 获取当前菜单，暂时只返回基类信息
+        /// </summary>
+        /// <param name="accessToken"></param>
+        /// <returns></returns>
+        public static GetMenuResult GetMenu(string accessToken)
+        {
+            var url = string.Format("https://api.weixin.qq.com/cgi-bin/menu/get?access_token={0}", accessToken);
+
+            var jsonString = HttpUtility.RequestUtility.HttpGet(url, Encoding.UTF8);
+            var finalResult = GetMenuFromJson(jsonString);
+            return finalResult;
+        }
+
+        /// <summary>
+        /// 从JSON字符串获取菜单对象
+        /// </summary>
+        /// <param name="jsonString"></param>
+        /// <returns></returns>
+        public static GetMenuResult GetMenuFromJson(string jsonString)
+        {
+            var finalResult = new GetMenuResult();
+
+            try
+            {
+                //@"{""menu"":{""button"":[{""type"":""click"",""name"":""单击测试"",""key"":""OneClick"",""sub_button"":[]},{""name"":""二级菜单"",""sub_button"":[{""type"":""click"",""name"":""返回文本"",""key"":""SubClickRoot_Text"",""sub_button"":[]},{""type"":""click"",""name"":""返回图文"",""key"":""SubClickRoot_News"",""sub_button"":[]},{""type"":""click"",""name"":""返回音乐"",""key"":""SubClickRoot_Music"",""sub_button"":[]}]}]}}"
+                object jsonResult = null;
+
+                JavaScriptSerializer js = new JavaScriptSerializer();
+                jsonResult = js.Deserialize<object>(jsonString);
+
+                var fullResult = jsonResult as Dictionary<string, object>;
+                if (fullResult != null && fullResult.ContainsKey("menu"))
+                {
+                    //得到菜单
+                    var menu = fullResult["menu"];
+                    var buttons = (menu as Dictionary<string, object>)["button"] as object[];
+
+                    foreach (var rootButton in buttons)
+                    {
+                        var fullButton = rootButton as Dictionary<string, object>;
+                        if (fullButton.ContainsKey("key") && !string.IsNullOrEmpty(fullButton["key"] as string))
+                        {
+                            //按钮，无下级菜单
+                            finalResult.menu.button.Add(GetSingleButtonFromJsonObject(fullButton));
+                        }
+                        else
+                        {
+                            //二级菜单
+                            var subButton = new SubButton(fullButton["name"] as string);
+                            finalResult.menu.button.Add(subButton);
+                            foreach (var sb in fullButton["sub_button"] as object[])
+                            {
+                                subButton.sub_button.Add(GetSingleButtonFromJsonObject(sb as Dictionary<string, object>));
+                            }
+                        }
+                    }
+                }
+                else if (fullResult != null && fullResult.ContainsKey("errmsg"))
+                {
+                    //菜单不存在
+                    throw new ErrorJsonResultException(fullResult["errmsg"] as string, null, null);
+                }
+            }
+            catch (ErrorJsonResultException ex)
+            {
+                finalResult = null;
+
+                //如果没有惨淡会返回错误代码：46003：menu no exist
+            }
+            catch (Exception ex)
+            {
+                //其他异常
+                finalResult = null;
+            }
+            return finalResult;
+        }
+
+        #endregion
+
+
+        /// <summary>
+        /// 删除菜单
+        /// </summary>
+        /// <param name="accessToken"></param>
+        /// <returns></returns>
+        public static WxJsonResult DeleteMenu(string accessToken)
+        {
+            var url = string.Format("https://api.weixin.qq.com/cgi-bin/menu/delete?access_token={0}", accessToken);
+            var result = Get.GetJson<WxJsonResult>(url);
+            return result;
+        }
+    }
+}
---
+++ Senparc.Weixin.MP/WeixinClient.cs
@@ -0,0 +1,122 @@
+﻿using Senparc.Weixin.MP.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Senparc.Weixin.MP
+{
+    public class WeixinClient
+    {
+        public WeixinClient()
+        {
+ 
+        }
+
+        public string TxtMenu(string menu)
+        {
+            string txt = "欢迎咨询，以下是菜单：请直接回复数字。\r\n[1]曼塔鳐鱼过山车\r\n[2]同一片海洋\r\n[3]蓝色地平线\r\n[4]与夏慕一起进餐\r\n[5]视频中心\r\n[6]全部图片";
+            return txt;
+        }
+
+        public string TxtWelcome()
+        {
+            string txt = "欢迎关注，以下是菜单：请直接回复数字。\r\n[1]曼塔鳐鱼过山车\r\n[2]同一片海洋\r\n[3]蓝色地平线\r\n[4]与夏慕一起进餐\r\n[5]视频中心\r\n[6]全部图片";
+            return txt;
+        }
+
+        public Article TxtMsg(string request)
+        {
+            if (request.Trim() == "1")
+            {
+                return DataCenter(1);
+            }
+            if (request.Trim() == "2")
+            {
+                return DataCenter(2);
+            }
+            if (request.Trim() == "3")
+            {
+                return DataCenter(3);
+            }
+            if (request.Trim() == "4")
+            {
+                return DataCenter(4);
+            }
+            if (request.Trim() == "logo")
+            {
+                return DataCenter(5);
+            }
+
+            return DataCenter(5);
+        }
+
+        private Article DataCenter(int num)
+        {
+            List<Article> ResponseMsgList = new List<Article>();
+
+            Article ResponseMsg = new Article();
+            ResponseMsg.Title = "曼塔鳐鱼过山车";
+            ResponseMsg.Description = "这是一幅美图，请您欣赏！";
+            ResponseMsg.PicUrl = "http://weixin.pescis.cn/seaworld/imgs/1.jpg";
+            ResponseMsg.Url = "http://www.seaworldparks.cn/orando/jingxian.html";
+
+            Article ResponseMsg2 = new Article();
+            ResponseMsg2.Title = "同一片海洋";
+            ResponseMsg2.Description = "这是一幅美图，请您欣赏！";
+            ResponseMsg2.PicUrl = "http://weixin.pescis.cn/seaworld/imgs/2.jpg";
+            ResponseMsg2.Url = "http://www.seaworldparks.cn/orando/biaoyan.html";
+
+            Article ResponseMsg3 = new Article();
+            ResponseMsg3.Title = "蓝色地平线";
+            ResponseMsg3.Description = "这是一幅美图，请您欣赏！";
+            ResponseMsg3.PicUrl = "http://weixin.pescis.cn/seaworld/imgs/3.jpg";
+            ResponseMsg3.Url = "http://www.seaworldparks.cn/orando/biaoyan.html";
+
+            Article ResponseMsg4 = new Article();
+            ResponseMsg4.Title = "与夏慕一起进餐";
+            ResponseMsg4.Description = "这是一幅美图，请您欣赏！";
+            ResponseMsg4.PicUrl = "http://weixin.pescis.cn/seaworld/imgs/18.jpg";
+            ResponseMsg4.Url = "http://www.seaworldparks.cn/orando/meishi.html";
+
+            Article ResponseMsg5 = new Article();
+            ResponseMsg5.Title = "视频中心";
+            ResponseMsg5.Description = "主题公园视频。";
+            ResponseMsg5.PicUrl = "http://weixin.pescis.cn/seaworld/imgs/1.jpg";
+            ResponseMsg5.Url = "http://weixin.pescis.cn/seaworld/v1.html";
+
+            ResponseMsgList.Add(ResponseMsg);
+            ResponseMsgList.Add(ResponseMsg2);
+            ResponseMsgList.Add(ResponseMsg3);
+            ResponseMsgList.Add(ResponseMsg4);
+            ResponseMsgList.Add(ResponseMsg5);
+
+            Article Msg;
+
+            switch(num)
+            {
+                case 1:
+                    Msg = ResponseMsgList[0];
+                    break;
+                case 2:
+                    Msg = ResponseMsgList[1];
+                    break;
+                case 3:
+                    Msg = ResponseMsgList[2];
+                    break;
+                case 4:
+                    Msg = ResponseMsgList[3];
+                    break;
+                case 5:
+                    Msg = ResponseMsgList[4];
+                    break;
+                default:
+                    Msg = ResponseMsgList[4];
+                    break;
+            }
+
+            return Msg;
+ 
+        }
+    }
+}
